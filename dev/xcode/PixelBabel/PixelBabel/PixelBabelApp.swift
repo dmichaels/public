4acbfde7e44a3ca0a8293adb1feb24d017d018d7
David Michaels <dmichaels@gmail.com>
2025-03-30 15:15:06 -0500

    pub

---
+++ dev/xcode/PixelBabel/PixelBabel/PixelBabelApp.swift
@@ -0,0 +1,29 @@
+//
+//  PixelBabelApp.swift
+//  PixelBabel
+//
+//  Created by David Michaels on 3/29/25.
+//
+
+import SwiftUI
+
+@main
+struct PixelBabelApp: App {
+    @State private var useGrayscale = false
+    @StateObject private var settings = AppSettings()
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environmentObject(settings)
+        }
+    }
+}
+
+class AppSettings: ObservableObject {
+    @Published var useGrayscale: Bool = false {
+        didSet { print("Grayscale changed: \(useGrayscale)") }
+    }
+    @Published var pixelSize: Int = 1 {
+        didSet { print("Pixel Size changed: \(pixelSize)") }
+    }
+}
---
+++ dev/xcode/PixelBabel/PixelBabelUITests/PixelBabelUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  PixelBabelUITestsLaunchTests.swift
+//  PixelBabelUITests
+//
+//  Created by David Michaels on 3/29/25.
+//
+
+import XCTest
+
+final class PixelBabelUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
